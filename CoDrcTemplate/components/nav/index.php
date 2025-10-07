4210c61a48f37ea0ae4a63426a9f5e9540281d9f
Matthew Zienert <mzienert@gmail.com>
2025-10-06 20:44:05 -0600

    dropdown styling

--- CoDrcTemplate/components/nav/index.php
+++ CoDrcTemplate/components/nav/index.php
@@ -17,7 +17,18 @@
 <nav class="main-nav">
     <ul class="nav-list">
         <?php foreach ($navItems as $item): ?>
-            <li class="nav-item"><a href="<?= htmlspecialchars($item['url']) ?>" class="nav-link"><?= htmlspecialchars($item['label']) ?></a></li>
+            <li class="nav-item <?= isset($item['children']) ? 'has-dropdown' : '' ?>">
+                <a href="<?= htmlspecialchars($item['url']) ?>" class="nav-link"><?= htmlspecialchars($item['label']) ?></a>
+                <?php if (isset($item['children'])): ?>
+                    <ul class="nav-dropdown">
+                        <?php foreach ($item['children'] as $child): ?>
+                            <li class="nav-dropdown-item">
+                                <a href="<?= htmlspecialchars($child['url']) ?>" class="nav-dropdown-link"><?= htmlspecialchars($child['label']) ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </li>
         <?php endforeach; ?>
     </ul>
 </nav>
