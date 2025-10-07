6bbc2d26ead8ebd16b7e7d039aa97094266aa157
Matthew Zienert <mzienert@gmail.com>
2025-10-06 20:33:25 -0600

    add children nav items

--- CoDrcTemplate/components/nav/index.php
+++ CoDrcTemplate/components/nav/index.php
@@ -2,7 +2,14 @@
 <?php
     $navItems = [
         ['label' => 'Home', 'url' => '/'],
-        ['label' => 'About', 'url' => '/about/'],
+        [
+            'label' => 'About',
+            'url' => '/about/',
+            'children' => [
+                ['label' => 'Our Team', 'url' => '/about/team/'],
+                ['label' => 'History', 'url' => '/about/history/']
+            ]
+        ],
         ['label' => 'Testing', 'url' => '/testing/'],
         ['label' => 'Contact', 'url' => '/contact/']
     ];
