920176d5eb814ed0cfe49dcf3cbb8a07c859bad3
Matthew Zienert <mzienert@gmail.com>
2025-10-08 11:12:20 -0600

    adding another nav child

--- CoDrcTemplate/data/nav.php
+++ CoDrcTemplate/data/nav.php
@@ -6,7 +6,8 @@
             'url' => '/about/',
             'children' => [
                 ['label' => 'Our Team', 'url' => '/about/team/'],
-                ['label' => 'History', 'url' => '/about/history/']
+                ['label' => 'History', 'url' => '/about/history/'],
+                ['label' => 'Testing2', 'url' => '/about/testing2/']
             ]
         ],
         ['label' => 'Testing', 'url' => '/testing/',],
