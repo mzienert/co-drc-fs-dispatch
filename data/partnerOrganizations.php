01bd5b5915f767e63a0955f55d40d453c5ef2e65
Matthew Zienert <mzienert@gmail.com>
2025-10-23 22:01:09 -0600

    partner logos

--- data/partnerOrganizations.php
+++ data/partnerOrganizations.php
@@ -8,55 +8,55 @@ return [
     [
         'name' => 'Bureau of Indian Affairs',
         'abbreviation' => 'BIA',
-        'image' => 'BIA_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/bia'
+        'image' => 'BIA.png',
+        'url' => 'https://www.bia.gov/'
     ],
     [
         'name' => 'Bureau of Land Management',
         'abbreviation' => 'BLM',
-        'image' => 'blm_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/blm'
+        'image' => 'blm.png',
+        'url' => 'https://www.blm.gov/'
     ],
     [
         'name' => 'Department of Defense',
         'abbreviation' => 'DOD',
-        'image' => 'DOD_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/dod'
+        'image' => 'DOD.png',
+        'url' => 'https://www.war.gov/'
     ],
     [
         'name' => 'US Fire Administration',
         'abbreviation' => 'USFA',
-        'image' => 'fireadministration_GS_noLine.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/usfa'
+        'image' => 'USFA.png',
+        'url' => 'https://www.usfa.fema.gov/'
     ],
     [
         'name' => 'US Fish and Wildlife Service',
         'abbreviation' => 'FWS',
-        'image' => 'FWS_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/fws'
+        'image' => 'FWS.png',
+        'url' => 'https://www.fws.gov/'
     ],
     [
         'name' => 'US Forest Service',
         'abbreviation' => 'USFS',
-        'image' => 'USFS_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/usfs'
+        'image' => 'USFS.png',
+        'url' => 'https://www.usfs.gov/'
     ],
     [
         'name' => 'National Association of State Foresters',
         'abbreviation' => 'NASF',
-        'image' => 'NASF_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/nasf'
+        'image' => 'NASF.gif',
+        'url' => 'https://www.stateforesters.org/'
     ],
     [
         'name' => 'National Oceanic and Atmospheric Administration',
         'abbreviation' => 'NOAA',
-        'image' => 'NOAA_GS.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/nws'
+        'image' => 'NOAA.jpg',
+        'url' => 'https://www.weather.gov'
     ],
     [
         'name' => 'National Park Service',
         'abbreviation' => 'NPS',
-        'image' => 'NPSArrowhead-FlatGS-4InchesHeight@300dpi.png',
-        'url' => 'https://www.nifc.gov/about-us/our-partners/nps'
+        'image' => 'NPS.png',
+        'url' => 'https://www.nps.goc'
     ]
 ];
