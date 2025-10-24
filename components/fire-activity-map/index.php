ab4b6c29c3a7e1b89a2e587e228489330e1105db
Matthew Zienert <mzienert@gmail.com>
2025-10-23 20:28:08 -0600

    get rid of magic string for arcgis url

--- components/fire-activity-map/index.php
+++ components/fire-activity-map/index.php
@@ -7,6 +7,24 @@
 	$height = Helpers::prop($props, 'height', '600px');
 	$showTitle = Helpers::prop($props, 'showTitle', true);
 	$basePath = Helpers::prop($props, 'dispatchInfo.base_path');
+
+	// Map configuration
+	$mapId = '65d822c03058408384069b0c94281d43';
+	$mapExtent = '-116.0948,36.9734,-90.807,44.5409'; // Rocky Mountain area bounds
+	$mapParams = [
+		'zoom' => 'true',
+		'previewImage' => 'false',
+		'scale' => 'true',
+		'legendlayers' => 'true',
+		'disable_scroll' => 'true',
+		'theme' => 'light'
+	];
+
+	// Build map URL
+	$mapUrl = 'https://nifc.maps.arcgis.com/apps/Embed/index.html?webmap=' . $mapId . '&extent=' . $mapExtent;
+	foreach ($mapParams as $key => $value) {
+		$mapUrl .= '&' . $key . '=' . $value;
+	}
 ?>
 
 <section id="fire-activity-map" class="fire-activity-map-section">
@@ -25,7 +43,7 @@
 			marginheight="0"
 			marginwidth="0"
 			title="RMCC Wildfire Intelligence Map - Website"
-			src="https://nifc.maps.arcgis.com/apps/Embed/index.html?webmap=65d822c03058408384069b0c94281d43&extent=-116.0948,36.9734,-90.807,44.5409&zoom=true&previewImage=false&scale=true&legendlayers=true&disable_scroll=true&theme=light"
+			src="<?= Helpers::sanitize($mapUrl) ?>"
 			allowfullscreen>
 		</iframe>
 	</div>
