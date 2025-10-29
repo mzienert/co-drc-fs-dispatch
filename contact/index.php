28b0b2aa93a4d1685b78e74ba4a7213c20162146
Matthew Zienert <mzienert@gmail.com>
2025-10-29 12:07:42 -0600

    updated contact page

--- contact/index.php
+++ contact/index.php
@@ -10,43 +10,48 @@
 
 <?php use App\Helpers; ?>
 
-<h1>Contact <?= Helpers::sanitize($dispatchInfo['name']) ?></h1>
-
 <div class="row content-row gtr-150">
     <div class="col-6 col-12-medium">
-        <section class="contact-info-section">
+        <section class="about-section">
             <h2>Contact Information</h2>
 
-            <div class="contact-details">
-                <h3><?= Helpers::sanitize($dispatchInfo['name']) ?></h3>
-                <p><?= Helpers::sanitize($dispatchInfo['id']) ?></p>
+            <div class="about-text">
+                <div class="row gtr-50">
+                    <div class="col-6 col-12-small">
+                        <p>
+                            <strong>24-Hour Line:</strong><br>
+                            <a href="tel:<?= Helpers::sanitize(preg_replace('/[^0-9]/', '', $dispatchInfo['phone_24_hour'])) ?>">
+                                <?= Helpers::sanitize($dispatchInfo['phone_24_hour']) ?>
+                            </a>
+                        </p>
+                    </div>
 
-                <p>
-                    <strong>24-Hour Line:</strong><br>
-                    <a href="tel:<?= Helpers::sanitize(preg_replace('/[^0-9]/', '', $dispatchInfo['phone_24_hour'])) ?>">
-                        <?= Helpers::sanitize($dispatchInfo['phone_24_hour']) ?>
-                    </a>
-                </p>
+                    <div class="col-6 col-12-small">
+                        <p>
+                            <strong>Office:</strong><br>
+                            <a href="tel:<?= Helpers::sanitize(preg_replace('/[^0-9]/', '', $dispatchInfo['phone_office'])) ?>">
+                                <?= Helpers::sanitize($dispatchInfo['phone_office']) ?>
+                            </a>
+                        </p>
+                    </div>
 
-                <p>
-                    <strong>Office:</strong><br>
-                    <a href="tel:<?= Helpers::sanitize(preg_replace('/[^0-9]/', '', $dispatchInfo['phone_office'])) ?>">
-                        <?= Helpers::sanitize($dispatchInfo['phone_office']) ?>
-                    </a>
-                </p>
+                    <div class="col-6 col-12-small">
+                        <p>
+                            <strong>Email:</strong><br>
+                            <a href="mailto:<?= Helpers::sanitize($dispatchInfo['email']) ?>">
+                                <?= Helpers::sanitize($dispatchInfo['email']) ?>
+                            </a>
+                        </p>
+                    </div>
 
-                <p>
-                    <strong>Email:</strong><br>
-                    <a href="mailto:<?= Helpers::sanitize($dispatchInfo['email']) ?>">
-                        <?= Helpers::sanitize($dispatchInfo['email']) ?>
-                    </a>
-                </p>
-
-                <p>
-                    <strong>Address:</strong><br>
-                    <?= Helpers::sanitize($dispatchInfo['address_line_1']) ?><br>
-                    <?= Helpers::sanitize($dispatchInfo['address_line_2']) ?>
-                </p>
+                    <div class="col-6 col-12-small">
+                        <p>
+                            <strong>Address:</strong><br>
+                            <?= Helpers::sanitize($dispatchInfo['address_line_1']) ?><br>
+                            <?= Helpers::sanitize($dispatchInfo['address_line_2']) ?>
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
